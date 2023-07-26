9be5ca6a8c5789c6cc0d0cde8c58b24951960e01
Audrey Dutcher <audrey@rhelmot.io>
2023-07-26 15:48:49 -0700

    Bump version to 1.0.3

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ if bytes is str:
 
 from setuptools import setup
 setup(name='nclib',
-      version='1.0.2',
+      version='1.0.3',
       python_requires='>=3.5',
       packages=['nclib'],
       scripts=['serve-stdio'],
